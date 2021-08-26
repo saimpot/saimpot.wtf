d5a52ba0a1aec705f0b7a230e336abcb085a0c7c
chatzipavlou <chatzipavlou@deventum.com>
2021-08-26 09:42:02 +0300

    Updated index and config files

--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,12 @@
   <div>
     <h1>saimpot.wtf</h1>
     <p>because it required a root element</p>
+    <p>testing a <NuxtLink to="test">asdf</NuxtLink></p>
   </div>
 </template>
+
+<script>
+export default {
+  transition: 'fade'
+}
+</script>
